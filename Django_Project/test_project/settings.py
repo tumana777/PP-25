f800c22290ac250f53c02dec2be8598b692eac94
tumana777 <tumana991@gmail.com>
2025-05-29 23:13:34 +0400

    remove smtp credentials

--- Django_Project/test_project/settings.py
+++ Django_Project/test_project/settings.py
@@ -158,6 +158,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'carprojectg@gmail.com'
-EMAIL_HOST_PASSWORD = 'fuzi ppww bmgn whjq'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
